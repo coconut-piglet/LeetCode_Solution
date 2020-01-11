5fccddfbf5856bd4b61b45380e3c23162af3f214
Richard <richard0410@sjtu.edu.cn>
2020-01-11 10:47:02 +0800

    [20/01/11][1]

---
+++ Algorithms/Two Sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // Solution 1 Brute Force
+        // for (int i = 0; i < nums.size(); i++) {
+        //     int toFind = target - nums[i];
+        //     for (int j = 0; j < nums.size(); j++) {
+        //         if (nums[j] == toFind && j != i) {
+        //             vector<int> ret = {i, j};
+        //             return ret;
+        //         }
+        //     }
+        // }
+		// Solution 2 Semi One-pass Hash Table
+        map<int, int> numsTable;
+        map<int, int>::iterator iter;
+        for (int i = 0; i < nums.size(); i++) {
+            numsTable.insert(pair(nums[i], i));
+            int toFind = target - nums[i];
+            iter = numsTable.find(toFind);
+            if (iter != numsTable.end()) {
+                if (iter->second == i) continue; // same number used twice
+                vector<int> ret = {iter->second, i};
+                return ret;
+            }
+        }
+        vector<int> emptyRet;
+        return emptyRet;
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# LeetCode_Solution
-寒假刷刷LeetCode
+# LeetCode Solution
+任爹说面试不刷LeetCode多半是送人头，所以就刷呗~
