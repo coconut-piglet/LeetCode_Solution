9d903630e7a94ec8f4027b57af4f70ec3c985a48
YUENIIMAA <yueniimaa@outlook.com>
2020-03-05 09:21:54 +0800

    [20/03/05][1]

--- Algorithms/#10 Regular Expression Matching.cpp
+++ Algorithms/#10 Regular Expression Matching.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
+        // Learnt from https://youtu.be/l3hda49XcDE
         int lenS = s.length();
         int lenP = p.length();
         char tokenS, tokenP;
         bool isMatch[lenS + 1][lenP + 1];
-        for (int i = 1; i <= lenP; i++) {
+		isMatch[0][0] = true; // empty s vs empty p, s is valid
+        for (int i = 1; i <= lenP; i++) { // empty s vs none empty p, mostly all s are invalid
             isMatch[0][i] = false;
         }
-        for (int i = 0; i <= lenS; i++) {
-            isMatch[i][0] = true;
+        for (int i = 1; i <= lenS; i++) { // none empty s vs empty p, all s are invalid
+            isMatch[i][0] = false;
         }
+		for (int i = 2; i <= lenP; i++) { // deal with a*b* alike situation
+			if (p[i - 1] == '*') {
+				isMatch[0][i - 1] = isMatch[0][i - 2];
+				isMatch[0][i] = isMatch[0][i - 2];
+			}
+		}
         for (int i = 1; i <= lenS; i++) {
             for (int j = 1; j <= lenP; j++) {
-                tokenS = s[i];
-                tokenP = p[j];
-                if (tokenP != '*') {
-                    isMatch[i][j] = isMatch[i - 1][j - 1] && (tokenS == tokenP);
-                }
-                else {
-                    isMatch[i][j] = isMatch[i][j - 2] || (isMatch[i - 1][j] && (tokenS == p[j - 1]));
-                }
+                tokenS = s[i - 1];
+                tokenP = p[j - 1];
+				if (tokenP == '.' || tokenS == tokenP) { // matches
+					isMatch[i][j] = isMatch[i - 1][j - 1];
+				}
+				else if (tokenP == '*') {
+					if (p[j - 2] == '.' || tokenS == p[j - 2]) {
+						isMatch[i][j] = isMatch[i][j - 2] || isMatch[i - 1][j]; // the word in front of '*' occurs 0+ times
+					}
+					else {
+						isMatch[i][j] = isMatch[i][j - 2]; // the word in front of '*' occurs 0 times
+					}
+				}
+				else {
+					isMatch[i][j] = false; // default false
+				}
             }
         }
         return isMatch[lenS][lenP];
