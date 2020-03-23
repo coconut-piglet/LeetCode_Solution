d513689bfd93bba8bd2fe1ffbfa54c0c6dc93dfc
YUENIIMAA <yueniimaa@outlook.com>
2020-03-24 00:08:25 +0800

    [20/03/24][1]

--- Algorithms/#58 Length of Last Word.cpp
+++ Algorithms/#58 Length of Last Word.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
+        int pos = s.length() - 1;
+        if (pos == -1) return 0;
+        int ret = 0;
+        while (s[pos] == ' ') {
+            pos--;
+            if (pos < 0) return 0;
+        }
+        while (s[pos] != ' ') {
+            ret++;
+            pos--;
+            if (pos < 0) break;
+        }
+        return ret;
     }
 };
