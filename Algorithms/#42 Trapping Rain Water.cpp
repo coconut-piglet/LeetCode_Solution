90411f15eb0b0e785a554329a6d0ab7f931bc931
YUENIIMAA <yueniimaa@outlook.com>
2020-04-04 16:59:15 +0800

    [20/04/04][1]

--- Algorithms/#42 Trapping Rain Water.cpp
+++ Algorithms/#42 Trapping Rain Water.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        
+        int ret = 0;
+        int left_position = 0, left_current = 0, left_max = 0;
+        int right_position = height.size() - 1, right_current = 0, right_max = 0;
+        while (left_position < right_position) {
+            left_current = height[left_position];
+            right_current = height[right_position];
+            if (left_current < right_current) {
+                if (left_current >= left_max) left_max = left_current;
+                else ret += (left_max - left_current);
+                left_position++;
+            }
+            else {
+                if (right_current >= right_max) right_max = right_current;
+                else ret += (right_max - right_current);
+                right_position--;
+            }
+        }
+        return ret;
     }
 };
