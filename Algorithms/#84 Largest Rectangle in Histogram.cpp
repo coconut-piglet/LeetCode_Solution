0b9ff8908dc08450473db75af30b0e2226856b1f
YUENIIMAA <yueniimaa@outlook.com>
2020-05-04 23:40:22 +0800

    [20/05/04][0.5]

--- Algorithms/#84 Largest Rectangle in Histogram.cpp
+++ Algorithms/#84 Largest Rectangle in Histogram.cpp
@@ -1,6 +1,43 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        return 0;
+        int size = heights.size();
+        
+        if (size == 0) return 0;
+        if (size == 1) return heights[0];
+        
+        int maxArea, height, width, area;
+        stack<int> prev;
+        
+        maxArea = 0;
+        heights.push_back(0);
+        size++;
+        
+        for (int i = 0; i < size; i++) {
+            if (prev.empty()) {
+                prev.push(i);
+                continue;
+            }
+            else if (heights[i] > heights[prev.top()]) {
+                prev.push(i);
+                continue;
+            }
+            else if (heights[i] < heights[prev.top()]) {
+                while (!prev.empty()) {
+                    height = heights[prev.top()];
+                    if (height < heights[i]) {
+                        break;
+                    }
+                    width = i - prev.top();
+                    area = height * width;
+                    maxArea = area > maxArea ? area : maxArea;
+                    prev.pop();
+                }
+                prev.push(i - width);
+                heights[i - width] = heights[i];
+                continue;
+            }
+        }
+        return maxArea;
     }
 };
