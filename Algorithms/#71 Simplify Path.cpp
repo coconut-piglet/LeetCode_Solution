b76406eecbfd7990748cf3cb95f3e55fdae03ecc
YUENIIMAA <yueniimaa@outlook.com>
2020-05-10 10:40:14 +0800

    [20/05/10][1][update]

--- Algorithms/#71 Simplify Path.cpp
+++ Algorithms/#71 Simplify Path.cpp
@@ -1,71 +1,26 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        int total, start, len;
+        int len;
         string dir;
         list<string> buf;
         
-        start = 1;
         path += '/';
-        total = path.length();
+        len = path.length();
         
-        for (int i = 1; i < total; i++) {
+        for (int i = 0; i < len; i++) {
             
-            if (path[i] == '/') {
-                len = i - start;
-                dir = path.substr(start, len);
-                start = i + 1;
-                
-                if (dir.length() == 0 || dir.compare(".") == 0) {
-                    continue;
-                }
-                else if (dir.compare("..") == 0) {
-                    if (!buf.empty()) buf.pop_back();
-                }
-                else {
-                    buf.push_back(dir);
-                }
+            if (path[i] != '/') {
+                dir += path[i];
             }
-            
-        }
-        
-        if (buf.empty()) return "/";
-        
-        string ret = "";
-        for (string name : buf) {
-            ret += "/";
-            ret += name;
-        }
-        
-        return ret;
-    }
-};class Solution {
-public:
-    string simplifyPath(string path) {
-        int total, start, len;
-        string dir;
-        list<string> buf;
-        
-        start = 1;
-        path += '/';
-        total = path.length();
-        
-        for (int i = 1; i < total; i++) {
-            
-            if (path[i] == '/') {
-                len = i - start;
-                dir = path.substr(start, len);
-                start = i + 1;
-                
-                if (dir.length() == 0 || dir.compare(".") == 0) {
-                    continue;
-                }
-                else if (dir.compare("..") == 0) {
+            else {
+                if (dir.compare("..") == 0) {
                     if (!buf.empty()) buf.pop_back();
                 }
-                else {
+                else if (dir.length() != 0 && dir.compare(".") != 0) {
                     buf.push_back(dir);
                 }
+                dir = "";
             }
             
         }
