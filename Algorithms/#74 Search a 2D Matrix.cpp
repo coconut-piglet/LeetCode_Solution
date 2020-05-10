cf270daeb9ad58694d41f10e259e8cf9b9b0211b
YUENIIMAA <yueniimaa@outlook.com>
2020-05-10 14:19:55 +0800

    [20/05/10][2][update]

--- Algorithms/#74 Search a 2D Matrix.cpp
+++ Algorithms/#74 Search a 2D Matrix.cpp
@@ -1,50 +1,33 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int numOfRow = matrix.size() - 1;
-        if (numOfRow < 0) return false;
-        int numOfCol = matrix[0].size() - 1;
-        if (numOfCol < 0) return false;
+        int numOfRow = matrix.size();
+        if (numOfRow == 0) return false;
+        int numOfCol = matrix[0].size();
+        if (numOfCol == 0) return false;
         
-        if (target < matrix[0][0] || target > matrix[numOfRow][numOfCol]) return false;
+        int begin, mid, end;
+        int midRow, midNum, midCol;
         
-        int low, mid, high;
-        low = 0;
-        high = numOfRow;
+        begin = 0;
+        end = numOfRow * numOfCol - 1;
         
-        while (low < high) {
-            mid = (low + high) >> 1;
-            if (target < matrix[mid][0]) {
-                high = mid - 1;
+        while (begin <= end) {
+            mid = (begin + end) >> 1;
+            midRow = mid / numOfCol;
+            midCol = mid % numOfCol;
+            midNum = matrix[midRow][midCol];
+            
+            if (target < midNum) {
+                end = mid - 1;
             }
-            else if (target > matrix[mid][numOfCol]){
-                low = mid + 1;
-            }
-            else {
-                low = mid;
-                break;
-            }
-        }
-        
-        int targetRow = low;
-        if (target < matrix[targetRow][0] || target > matrix[targetRow][numOfCol]) return false;
-        
-        low = 0;
-        high = numOfCol;
-        
-        while (low <= high) {
-            mid = (low + high) >> 1;
-            if (target < matrix[targetRow][mid]) {
-                high = mid - 1;
-            }
-            else if (target > matrix[targetRow][mid]) {
-                low = mid + 1;
+            else if (target > midNum) {
+                begin = mid + 1;
             }
             else {
                 return true;
             }
         }
-        
         return false;
     }
 };
