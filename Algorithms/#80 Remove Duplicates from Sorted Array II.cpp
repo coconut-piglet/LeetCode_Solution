95fabbe00c64cb6bf0c6c163ebf957ea30d94280
YUENIIMAA <yueniimaa@outlook.com>
2020-05-11 10:00:40 +0800

    [20/05/11][1][update]

--- Algorithms/#80 Remove Duplicates from Sorted Array II.cpp
+++ Algorithms/#80 Remove Duplicates from Sorted Array II.cpp
@@ -4,31 +4,19 @@ public:
         int size = nums.size();
         if (size <= 2) return size;
         
-        int dst, src, crt, prev;
-        bool dup;
-        
-        dst = 1;
-        src = 1;
-        prev = nums[0];
-        dup = false;
+        int prv = 0;
+        int src = 2;
+        int dst = 2;
         
         while (src < size) {
-            crt = nums[src];
-            if (crt == prev) {
-                if (dup) {
-                    src++;
-                    continue;
-                }
-                dup = true;
+            if (nums[src] != nums[prv]) {
+                nums[dst] = nums[src];
+                dst++;
+                prv++;
             }
-            else {
-                prev = crt;
-                dup = false;
-            }
-            nums[dst] = crt;
-            dst++;
             src++;
         }
+        
         size = dst;
         
         return size;
