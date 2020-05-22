ea29d78b2b8a4390e43da7dff5b6dbdc10ec7f7e
YUENIIMAA <yueniimaa@outlook.com>
2020-05-22 13:24:25 +0800

    [20/05/21][1][update]

--- Algorithms/#93 Restore IP Addresses.cpp
+++ Algorithms/#93 Restore IP Addresses.cpp
@@ -12,32 +12,26 @@ private:
     int len;
     string inputStr;
     void solveProblem(string & outputIp, int currentNum, int src, int dst, int leftNum, vector<string> &ret) {
-        if (leftNum == 0) {
-            cout << outputIp;
-            if (src == len) {
+        if (leftNum == 0) { 
+            if (src == len) { // exit 0: a valid ip address
                 ret.push_back(outputIp);
+                return;
             }
-            else {
+            else { // exit 1: input string not used up
                 return;
             }
         }
-        if (src >= len) {
+        if (src >= len) { // exit 2: input string too short
             return;
         }
+        
         currentNum = currentNum * 10 + inputStr[src] - '0';
-        if (currentNum == 0) { // case .0. should jump to next number
+        
+        if (currentNum < 256) {
             outputIp[dst] = inputStr[src];
-            outputIp[dst + 1] = '.';
+            if (leftNum > 1) outputIp[dst + 1] = '.';
             solveProblem(outputIp, 0, src + 1, dst + 2, leftNum - 1, ret);
-        }
-        else if (currentNum > 255) { // case .256. or .1234. should stop expanding current number
-            return;
-        }
-        else { // .XX. calculate next number and then expand current number
-            outputIp[dst] = inputStr[src];
-            outputIp[dst + 1] = '.';
-            solveProblem(outputIp, 0, src + 1, dst + 2, leftNum - 1, ret);
-            solveProblem(outputIp, currentNum, src + 1, dst + 1, leftNum, ret);
+            if (currentNum > 0) solveProblem(outputIp, currentNum, src + 1, dst + 1, leftNum, ret);
         }
     }
 };
